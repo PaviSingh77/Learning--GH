dc82aaf9e6c53f664df73649c2f98e580cd46620
Pavi <pavis0727@gmail.com>
2021-12-13 11:24:07 +1100

    Initial Commit

---
+++ LEARNING GITHUB/LEARNING_GITHUBApp.swift
@@ -0,0 +1,17 @@
+//
+//  LEARNING_GITHUBApp.swift
+//  LEARNING GITHUB
+//
+//  Created by Pavi on 13/12/21.
+//
+
+import SwiftUI
+
+@main
+struct LEARNING_GITHUBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
